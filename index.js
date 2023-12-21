85951131efbf64f6d0d3783bada5a510798f6f30
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2023-12-21 13:26:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 import readlineSync from 'readline-sync';
 
-function getUserChoice() {
-    const choices = ["камень", "ножницы", "бумага"];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+            
+function getComputerChoice() {
+    const choices = ["Камень", "Ножницы", "Бумага"];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
+
+function getUserChoice(){
+    console.log('Выберите Вашу фигуру:\n1. Камень\n2. Ножницы\n3.Бумага');
+    const choiceIndex = readlineSync.keyIn('Ваш выбор: ', {limit: '$<1-3>'}) - 1;
+    const choices = ['Камень', 'Ножницы', 'Бумага'];
+    return cnoices[choiceIndex];
+}
 function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        return "Ничья!";
-    }
-    if (userChoice === "Камень") {
-        return computerChoice === "Ножницы" ? "Вы победили! Камень ломает ножницы." : "Вы проиграли! Бумага заворачивает камень.";
-    }
-    if (userChoice = "Ножницы") {
-        return computerChoice === "Бумага" ? "Вы победили! Ножницы режут бумагу." : "Вы проиграли! Камень ломает ножницы.";
-    }
-    if (userChoice === "Бумага") {
-        return computerChoice === "Камень" ? "Вы победили! Бумага заворачивает камень." : "Вы проиграли! Ножницы режут бумагу.";
-    }
-}
-
-function getUserChoice(choice) {
-    choice = choice.toLowerCase();
-    if (choice === "камень" || choice === "ножницы" || choice === "бумага") {
-        return choice;
-    } else {
-        return "Неверный ввод";
-    }
-}
-
-function playGame() {
-    const userChoice = getUserChoice(prompt("Выберите вашу фигуру: Камень, Ножницы или Бумага"));
-    const computerChoice = getComputerChoice();
-    console.log(`Ваш выбор: ${userChoice}`);
+    console.log(`Вы выбрали: ${userChoice}`);
     console.log(`Компьютер выбрал: ${computerChoice}`);
-    console.log(`Результат: ${determineWinner(userChoice, computerChoice)}`);
-    const playAgain = prompt("Хотите сыграть еще раз? (да/нет)");
-    if (playAgain.toLowerCase() === "да") {
-        playGame;
-    } else {
-        console.log("Спасибо за игру! До встречи!");
+    
+    if (userChoice === computerChoice) {
+        return "Результат: Ничья!";
     }
-}
+    if (userChoice === "Камень" && computerChoice === "Ножницы") {
+        return 'Результат: Вы победили! Камень ломает ножницы.';
+    }
+    if (userChoice === "Камень"  && computerChoice === "Бумага") {
+        return 'Результат: Вы проиграли! Бумага заворачивает камень.';
+    }
+    if (userChoice === "Ножницы" && computerChoice === "Камень") {
+        return 'Результат: Вы проиграли! Камень ломает ножницы.';
+    }
+    if (userChoice === "Ножницы" && computerChoice === "Бумага") {
+        return 'Результат: Вы победили! Ножницы режут бумагу.';
+    }
+    if (userChoice === "Бумага" && computerChoice === "Ножницы") {
+        return 'Результат: Вы проиграли! Ножницы режут бумагу.';
+    }
+    if (userChoice === "Бумага" && computerChoice === "Камень") {
+        return 'Результат: Вы победили! Бумага заворачивает камень';
+    }
+}    
 
-console.log("Добро пожаловать в игру 'Камень, Ножницы, Бумага'!");
-playGame();
+    function playGame() {
+        console.log('Добро пожаловать в игру "Камень, Ножницы, Бумага"!\n');
+        let playAgain = 'да';
+        while (playAgain.toLowerCase() === 'да') {
+            const userChoice = getUserChoice();
+            const computerChoice = getComputerChoice();
+            determineWinner(userChoice, computerChoice);
+            playAgain = readlineSync.question('\nХотите сыграть еще раз? (да/нет): ');
+        }
+        console.log('\nСпасибо за игру! До встречи!');
+    }
+   playGame();
